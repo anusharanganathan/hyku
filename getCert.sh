deca246bf46dd3fb2f04e906a05d9f0dde90d5f8
Raja Nandakumar <raja.nandakumar@cern.ch>
2018-08-12 23:19:59 +0100

    Add certbot in docker-compose

--- getCert.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -it --rm -v hyku_certs:/etc/letsencrypt -v hyku_certs-data:/data/letsencrypt certbot/certbot certonly --test-cert --webroot --webroot-path=/data/letsencrypt -d 'up2.digitalnest.co.uk,repo1.up2.digitalnest.co.uk,repo2.up2.digitalnest.co.uk'
