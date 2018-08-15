94041c655b17ef1206929cd81231542241927163
Anusha Ranganathan <anusha3@gmail.com>
2018-08-15 15:21:44 +0100

    Created a docker dir to hold docker related files

--- docker/certbot/manage_certs.sh
+++ docker/certbot/manage_certs.sh
@@ -6,7 +6,8 @@ then
   docker exec hyku_web_server_1 cp /tmp/nginx_ssl.conf /etc/nginx/conf.d/default.conf
   echo "Reload nginx"
   docker exec hyku_web_server_1 nginx -s reload
-  rm /data/letsencrypt/cert_installed
+  # Do not delete status file. We need the config file copied
+  # rm /data/letsencrypt/cert_installed
   # Start cron to renew certificate. Run certbot renew using certbot service
   # If cert_renew_status, reload nginx
 fi
