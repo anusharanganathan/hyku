2d3f0353c04f02803ef9b0394fd234b46d34482c
Anusha Ranganathan <anusha3@gmail.com>
2018-08-27 09:36:35 +0100

    Updated nginx setup scripts for conditional installation of ssl ca and sshibboleth based on environment variables

---
+++ docker/web_server_env.sh
@@ -0,0 +1,7 @@
+export DOMAIN=up2.digitalnest.co.uk
+export TENANTS="repo1 repo2"
+export EMAIL=test@digitalnest.co.uk
+export USE_TEST_CERT=true
+export USE_SHIBBOLETH=true
+export USE_LETS_ENCRYPT=false
+export SHIBBOLETH_TENANT=repo1
